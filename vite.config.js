e4a4973310cee546674fef34c49b4888005a01ca
RobFrontend <robert.grabowski97@gmail.com>
2024-02-01 00:24:25 +0100

    img6

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,4 @@ import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint],
-  base: "/",
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-  },
 });
